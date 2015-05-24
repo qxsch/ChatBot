a1e53958073adcbe5cdd644dc669e0db34ad69f6
qxsch <qxsch@users.noreply.github.com>
2015-05-24 11:26:53 +0200

    first unit tests

---
+++ BotTests/LinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QXS.ChatBot;
+
+namespace QXS.ChatBot.Tests
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+
+        [TestMethod]
+        public void TestCount()
+        {
+            LinkedList<int> liste = new LinkedList<int>();
+            Assert.AreEqual(0, liste.Count, "Count Method returns a wrong result.");
+
+            for (int i = 1; i <= 3; i++)
+            {
+                liste.Push(i);
+                Assert.AreEqual(i, liste.Count, "Count Method returns a wrong result.");
+            }
+             
+        }
+    }
+}
